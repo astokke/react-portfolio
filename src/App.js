e8b1cca48fdda22adf4c5ca3c662c829d6b9eaa5
anst <andre.stokke@gmail.com>
2018-01-24 18:07:33 +0100

    Initialize React.js project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
+class App extends Component {
+  render() {
+    return (
+      <p> boom </p>
+    );
+  }
+}
+
+export default Root;
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <p> boom </p>
+    );
+  }
+}
+
+export default Header;
