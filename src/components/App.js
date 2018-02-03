572b6675886e87231cb7fc43cacc863e30a92f28
anst <andre.stokke@gmail.com>
2018-02-03 01:15:41 +0100

    gh-pages

--- src/components/App.js
+++ src/components/App.js
@@ -23,7 +23,7 @@ class App extends Component {
   render() {
     return (
       <div>
-      <Router>
+      <Router basename="/react-portfolio/">
         <div className="main">
         <Header/>
             <Switch>
